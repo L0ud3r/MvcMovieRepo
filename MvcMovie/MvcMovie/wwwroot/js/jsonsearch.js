11d315fb521985243072032683023ba455241ded
Pedro Simões <69718437+L0ud3r@users.noreply.github.com>
2023-02-15 13:13:16 +0000

    Tentativa fix search bar

---
+++ MvcMovie/MvcMovie/wwwroot/js/jsonsearch.js
@@ -0,0 +1,22 @@
+﻿function search() {
+    $('#search-form').on('submit', function (e) {
+        e.preventDefault(); // prevent default form submission behavior
+
+        // Send an AJAX request to the server to get the filtered data for the table
+        $.ajax({
+            url: '/Movies/index',
+            type: 'GET',
+            data: {
+                searchString: $('#movie').val(),
+                movieGenre: $('#genre').val()
+            },
+            success: function (data) {
+                // Update the table with the new data
+                $('#table').bootstrapTable('load', data);
+            },
+            error: function (xhr) {
+                console.log(xhr);
+            }
+        });
+    });
+};
